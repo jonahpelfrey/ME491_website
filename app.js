a196e6c9683db549b453df282a0647b3bd0b09a2
Jonah Pelfrey <jsp916@gmail.com>
2016-12-21 12:41:04 -0600

    second publish

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-var app = angular.module("badgerloop", ['ui.bootstrap']);
+var app = angular.module("badgerloop", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+	$routeProvider
+	.when("/", {
+		templateUrl: "home.html"
+	})
+	.when("/braking", {
+		templateUrl: "braking.html"
+	})
+	.when("/electrical", {
+		templateUrl: "electrical.html"
+	})
+	.when("/levitation", {
+		templateUrl: "levitation.html"
+	})
+	.when("/vaccuum", {
+		templateUrl: "vaccuum.html"
+	})
+	.when("/contact", {
+		templateUrl: "contact.html"
+	})
+	.otherwise({
+		templateUrl: "home.html"
+	});
+});
 
